8c80bd4ceccc5759487913fccd170153fe9c5fbc
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2025-05-05 20:10:32 -0300

    busca de contato usando for

---
+++ JavaScript/teste.js
@@ -0,0 +1,7 @@
+const user = ['Maria', 'João', 'Pedro', 'Ana', 'Lucas', 'Julia', 'Carlos', 'Fernanda', 'Roberto', 'Patrícia'];
+
+for (let i = 0; i < user.length; i++) {
+  console.log(user[i]);
+  console.log(i+1);
+  
+}
