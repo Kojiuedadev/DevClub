bb65e4d44fb80bcc8f48e66cdd3fcae966ce4fc9
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2025-07-14 21:24:09 -0300

    Add styled components for user registration form and global styles

--- Projetos/projetos-react/projeto-cadastro-usuario/src/App.jsx
+++ Projetos/projetos-react/projeto-cadastro-usuario/src/App.jsx
@@ -1,9 +1,51 @@
-function App() {
+import {
+  Container,
+  Title,
+  TopBackground,
+  Form,
+  ContainerInputs,
+  Input,
+  InputLabel,
+  Button,
+} from "./styles";
+
+import UsersImage from "./assets/users.png";
+
+function Home() {
   return (
-    <div>
-      <h1>Vite + React</h1>
-    </div>
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt="imagem-usuario" />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuário</Title>
+        <ContainerInputs>
+          <div>
+            <InputLabel>
+              Nome <span>*</span>
+            </InputLabel>
+            <Input type="text" placeholder="Nome do usuário" />
+          </div>
+
+          <div>
+            <InputLabel>
+              Idade <span>*</span>
+            </InputLabel>
+            <Input type="number" placeholder="Idade do usuário" />
+          </div>
+        </ContainerInputs>
+
+        <div style={{ width: "100%" }}>
+          <InputLabel>
+            E-mail <span>*</span>
+          </InputLabel>
+          <Input type="email" placeholder="E-mail do usuário" />
+        </div>
+        <Button>Cadastrar usuário</Button>
+      </Form>
+    </Container>
   );
 }
 
-export default App;
+export default Home;
