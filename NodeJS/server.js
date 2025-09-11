e550595f2014d2eb43c03ac87e8a6daf1a7aa46b
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2025-09-10 21:32:35 -0300

    realizando a conexao com mongoDB e adicionando usuarios com Post

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,21 +1,34 @@
-//Subindo servidor
-//Modo antigo:
-//const http = require('express');
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-//modo novo:
-import express from 'express'; //importa o express
-const app = express(); //cria uma aplicação express
+const app = express();
+const prisma = new PrismaClient();
 
-app.get('/usuario', (req, res) => { //cria uma rota GET
-  res.send('Rota de usuário');//responde com uma mensagem
+app.use(express.json());
+
+const users = [];
+
+app.get("/usuario/", (req, res) => {
+  //cria uma rota GET
+
+  res.status(200).json(users);
+}); //responde com o array de usuários
+
+app.post("/usuario", async (req, res) => {//cria uma rota POST
+  
+  await prisma.user.create({ //cria um novo usuário no banco de dados
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  
+  res.status(201).json({ message: "Usuário salvo com sucesso!" }); //responde com uma mensagem
 });
 
-app.post('/usuario', (req, res) => { //cria uma rota POST
-  res.send('Usuário salvo com sucesso!');//responde com uma mensagem
-});
-
-
-
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));//inicia o servidor na porta 3000
+app.listen(3000, () => console.log("Servidor rodando na porta 3000")); //inicia o servidor na porta 3000
 
 //http://localhost:3000/
+//Username : alekueda_db_user
+//Password : XPKXIXJRVEL3FNbn
