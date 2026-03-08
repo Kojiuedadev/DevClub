263ffe28c12002c7573edc44799ed2826c1c8245
Kojiuedadev <alexandre.ueda.dev@gmail.com>
2026-03-08 18:56:14 -0300

    configurando o express e usando rota Get

---
+++ 03-nodejs/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/usuarios", (req, res) => {
+  res.send("Lista de usuarios");
+});
+
+app.listen(3000);
+
+// http://localhost:3000/
